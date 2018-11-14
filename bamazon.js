5b62fbf8bbe8cc8212f539b8370d9f0394448ae2
gerardo dubcovsky <pablodubco@gmail.com>
2018-11-13 23:30:27 -0600

    Added connections to bamazon.js

--- bamazon.js
+++ bamazon.js
@@ -5,3 +5,16 @@ var itemArray = [];
 var priceArray = [];
 var connection;
 
+function createConnection() {
+    connection = mysql.createConnection({
+        host: "localhost",
+        // Your port; if not 3306
+        port: 3306,
+        // Your username
+        user: "root",
+        // Your password
+        password: "Dam3Acc3soAMySQL",
+        database: "bamazon"
+    });
+}
+
